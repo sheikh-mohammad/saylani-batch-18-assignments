0fb9ac47ea02487ca8454d5b738ce465ac4fa42a
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-31 22:53:01 +0500

    added solution

---
+++ assignment-01/script.js
@@ -0,0 +1,26 @@
+let studentNames = [
+    "Faheem",
+    "Nasir",
+    "Hammad",
+    "Hamza",
+    "Abdullah",
+    "Ali",
+    "Rizwan",
+    "Ahmed",
+    "Bilal",
+    "Usman",
+    "Ayaan",
+    "Saad",
+    "Zain",
+    "Huzaifa",
+    "Talha",
+    "Danish",
+    "Areeb",
+    "Sameer",
+    "Fahad",
+    "Ibrahim"
+]; 
+
+for (var i = 0; i < studentNames.length; i++) {
+    document.writeln(`Student ${i} ${studentNames[i]} <br />`)
+}
