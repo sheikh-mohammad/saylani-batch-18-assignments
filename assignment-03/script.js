1fb1bcff426482ead4980dff775e5b220d8bd29c
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-23 19:27:49 +0500

    Added number of passed and failed students

--- assignment-03/script.js
+++ assignment-03/script.js
@@ -1,12 +1,18 @@
 var marks = [85, 72, 66, 90, 55, 40, 35, 28, 20, 10];
+var passedStudents = 0;
+var failedStudents = 0;
 
 function calculateResult() {
   for (var i = 0; i < marks.length; i++) {
     var mark = marks[i];
     if (mark >= 40) {
       document.writeln("Marks: " + mark + " - Grade: Pass" + "<br>");
+      passedStudents++;
     } else {
       document.writeln("Marks: " + mark + " - Grade: Fail" + "<br>");
+      failedStudents++;
     }
   }
+  document.writeln("Total Passed Students : " + passedStudents + "<br>");
+  document.writeln("Total Failed Students : " + failedStudents + "<br>");
 }
