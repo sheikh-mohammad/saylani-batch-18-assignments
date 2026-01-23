a68feb47ec2d1ec2f1f188ef98a5c2b996fa22c1
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-23 19:26:01 +0500

    Implementted grading logic

--- assignment-03/script.js
+++ assignment-03/script.js
@@ -1,1 +1,12 @@
 var marks = [85, 72, 66, 90, 55, 40, 35, 28, 20, 10];
+
+function calculateResult() {
+  for (var i = 0; i < marks.length; i++) {
+    var mark = marks[i];
+    if (mark >= 40) {
+      document.writeln("Marks: " + mark + " - Grade: Pass" + "<br>");
+    } else {
+      document.writeln("Marks: " + mark + " - Grade: Fail" + "<br>");
+    }
+  }
+}
