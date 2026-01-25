9b90fea1863163d9b75b19dd2d2c2301807dfdd4
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-25 08:11:32 +0500

    Assignment 4 completed

--- assignment-04/script.js
+++ assignment-04/script.js
@@ -1,4 +1,8 @@
 function addNumbers(num1, num2) {
-  sum = num1 + num2;
+  var sum = num1 + num2;
   return sum;
 }
+
+var result = addNumbers(10, 20);
+
+document.writeln("<h1>Sum is: " + result + "</h1>");
