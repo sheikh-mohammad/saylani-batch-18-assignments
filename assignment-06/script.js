8b4eeacbd02d83fb6299a54376fe54a1d4605ffb
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-02 19:34:21 +0500

    Added calculate func

--- assignment-06/script.js
+++ assignment-06/script.js
@@ -2,3 +2,10 @@ function extractNumber(value) {
     var displayValue = document.getElementById("display-value");
     displayValue.innerHTML += value
 }
+
+function calculate() {
+    var displayValue = document.getElementById("display-value");
+    var result = eval(displayValue.innerHTML);
+
+    displayValue.innerHTML = result
+}
